5486223aa9448ac5c169c3ccb54b1ae8f328cea8
Delapouite <delapouite@gmail.com>
2023-11-18 15:22:20 +0100

    feat: add check-svgs.sh script and fix non-conforming files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Game Icons
 
-All SVG icons available on http://game-icons.net
+All SVG icons available on https://game-icons.net
 
 Also, this repository is used to [track](https://github.com/game-icons/icons/issues) requests made on Twitter or by email.
 You can add your own suggestions.
@@ -8,6 +8,10 @@ You can add your own suggestions.
 ## Contribution
 
 Contributions are welcomed! Have a look at this [document](https://github.com/game-icons/icons/blob/master/CONTRIBUTING.md) for help.
+
+## Scripts
+
+A few shell-scripts are at your disposal for various batch tasks.
 
 ## License
 
---
+++ check-svgs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# this script acts as a crude linter to check if some SVG files deviate from the intended format
+
+SVG_TAIL='z"/></svg>'
+
+ICON_HEAD='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 0h512v512H0z"/><path fill="#fff" d="'
+rg --invert-match --count-matches "$ICON_HEAD(.*)$SVG_TAIL" **/*.svg | rg --invert-match 'badges/'
+
+BADGE_HEAD='<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><circle cx="128" cy="128" r="128"/><circle stroke="#fff" stroke-width="18" cx="128" cy="128" r="101"/><path fill="#fff" d="'
+rg --invert-match --count-matches "$BADGE_HEAD(.*)$SVG_TAIL" badges/*.svg | rg --invert-match 'blank'
